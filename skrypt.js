359c9ba69a79b256fcb2d9bc61c210a8eb8afa3b
maciek <augustyniak.maciej1990@gmail.com>
2020-09-27 13:24:17 +0200

    javascript zadanie 14

--- skrypt.js
+++ skrypt.js
@@ -1,19 +1,23 @@
-let var1 = 1;
-let var2 = 1;
-if(var1 == var2){
-    console.log('var1 == var2');
-}
-if(va1 === var2){
-conlse.log('var1 === va2');
-}
+// let var1 = 1;
+// let var2 = 1;
+// if (var1 == var2) {
+//     console.log('var1 == var2');
+// }
+// if (va1 === var2) {
+//     conlse.log('var1 === va2');
+// }
+//
+// var1 = 1;
+// var1 = '1'
+// if (var1 == var2) {
+//     console.log('var1 == var2');
+// }
+// if (var1 === var2) {
+//     conlse.log('var1 === va2');
+// } else {
+//     console.log('var1 !== var2');
+// }
 
-var1=1;
-var1='1'
-if(var1 == var2){
-    console.log('var1 == var2');
-}
-if(var1 === var2) {
-    conlse.log('var1 === va2');
-} else{
-    console.log('var1 !== var2');
+function alertFunction(){
+    alert("Test");
 }
