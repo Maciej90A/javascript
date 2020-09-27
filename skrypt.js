466503968c3268ec0aebd9e0e1af5ce2edf785e6
maciek <augustyniak.maciej1990@gmail.com>
2020-09-27 14:12:01 +0200

    javascript zadanie 16

--- skrypt.js
+++ skrypt.js
@@ -1,3 +1,4 @@
+//12
 // let var1 = 1;
 // let var2 = 1;
 // if (var1 == var2) {
@@ -18,15 +19,26 @@
 //     console.log('var1 !== var2');
 // }
 
+//13
 // function alertFunction(){
 //     alert("Test");
 // }
 
-function swap() {
-    let barFoo = document.getElementById("foobar");
-    if (barFoo.innerHTML === "Foo bar!") {
-        barFoo.innerHTML = "Bar foo!";
-    } else {
-        barFoo.innerHTML = "Foo bar!";
+//15
+// function swap() {
+//     let barFoo = document.getElementById("foobar");
+//     if (barFoo.innerHTML === "Foo bar!") {
+//         barFoo.innerHTML = "Bar foo!";
+//     } else {
+//         barFoo.innerHTML = "Foo bar!";
+//     }
+// }
+
+//16
+function validateForm() {
+    let formElement = document.forms['contactForm']['firstname'].value;
+    if (formElement === '') {
+        alert('Name must be filled out.');
+        return false;
     }
 }
