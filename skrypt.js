f0914f20072fad761def1fc580dd305a123b2999
maciek <augustyniak.maciej1990@gmail.com>
2020-09-27 13:14:45 +0200

    javascript zadanie 13

--- skrypt.js
+++ skrypt.js
@@ -1,2 +1,19 @@
-alert("hello World!");
-console.log("Hello World!");
+let var1 = 1;
+let var2 = 1;
+if(var1 == var2){
+    console.log('var1 == var2');
+}
+if(va1 === var2){
+conlse.log('var1 === va2');
+}
+
+var1=1;
+var1='1'
+if(var1 == var2){
+    console.log('var1 == var2');
+}
+if(var1 === var2) {
+    conlse.log('var1 === va2');
+} else{
+    console.log('var1 !== var2');
+}
